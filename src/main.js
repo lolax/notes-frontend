f68137710071f11414080d9da87fe588a2966e48
Lola <lolahef@gmail.com>
2019-03-16 17:20:54 -0400

    Firebase setup

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
+import firebase from 'firebase/app'
 import VueRouter from 'vue-router'
 import App from './App'
 import router from './router'
+import config from './config'
 
 Vue.use(VueRouter)
 
 Vue.config.productionTip = false
+
+firebase.initializeApp(config)
 
 new Vue({
   router: router,

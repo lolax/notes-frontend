e60258923b3e4d49274f4664c1aac28b2c3e4fca
Lola <lolahef@gmail.com>
2019-04-03 22:12:34 -0400

    Removed old endpoints

--- src/mixins.js
+++ src/mixins.js
@@ -5,7 +5,6 @@ export const global = {
         editNote(id, changes) {  
             axios
                 .put(`https://lola-notes-server.herokuapp.com/restricted/notes/${id}`, changes,  {headers: { Authorization: localStorage.getItem("token") }})
-                // .put(`http://localhost:3300/restricted/notes/${id}`, changes,  {headers: { Authorization: localStorage.getItem("token") }})
                 .then(res => (this.notes = this.notes.map(note => note.id === id ? res.data : note)))
                 .catch(err => (console.log(err)))
                 .then(() => this.$root.$emit("reloadResources"))
@@ -16,7 +15,6 @@ export const global = {
         deleteNote(id) {
             axios
                 .delete(`https://lola-notes-server.herokuapp.com/restricted/notes/${id}`,  {headers: { Authorization: localStorage.getItem("token") }})
-                // .delete(`http://localhost:3300/restricted/notes/${id}`,  {headers: { Authorization: localStorage.getItem("token") }})
                 .then(res => (this.notes = res.data))
                 .catch(err => (console.log(err)))
                 .then(() => this.$root.$emit("reloadResources"))
@@ -26,8 +24,7 @@ export const global = {
             const clone = { title, content }
             axios  
                 .post("https://lola-notes-server.herokuapp.com/restricted/notes", clone,  {headers: { Authorization: localStorage.getItem("token") }})
-                // .post("http://localhost:3300/restricted/notes", clone,  {headers: { Authorization: localStorage.getItem("token") }})
-                .then(res => (this.message = "Note cloned"))
+                .then(() => (this.message = "Note cloned"))
                 .catch(err => (console.log(err)))
                 .then(() => this.$root.$emit("reloadResources"))
 
